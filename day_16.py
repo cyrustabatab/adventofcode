1c3688c25c15e5bd0af6159a161b2b453e9bc0a2
Cyrus Taba <24766642+cyrustabatab@users.noreply.github.com>
2022-01-06 22:47:05 -0800

    work

--- day_16.py
+++ day_16.py
@@ -1,7 +1,8 @@
+import math
 
+INPUT_FILE = "input_16.txt"
 
-
-def day_16():
+def day_16(input_file=INPUT_FILE):
     
     
     hex_to_binary = {}
@@ -13,9 +14,68 @@ def day_16():
         bits = bits[index +1:].zfill(4)
         hex_code =hex_code[hex_index +1:].upper()
         hex_to_binary[hex_code] = bits
+    
+
+    with open(input_file,'r') as f:
+        hexadecimal_string = f.read().strip()
+
+    binary_string = [] 
+    for character in hexadecimal_string:
+        binary = hex_to_binary[character]
+        binary_string.append(binary)
 
     
-    print(hex_to_binary)
+    binary_string = ''.join(binary_string)
+
+
+    i = 0
+
+    version_numbers = 0
+    while i < len(binary_string):
+
+        version_number = binary_string[i:i+3]
+        version_numbers += int(version_number,2)
+        i += 3
+        packet_type = int(binary_string[i:i+3],2)
+        i += 3
+
+        if packet_type == 4:
+            i += 1
+            length = 11
+            while binary_string[i] == '1':
+                i += 5
+                length += 5
+
+            i += 5 
+            
+            added_zero_bits = math.ceil(length / 4) * 4 - length
+            i += added_zero_bits
+        elif packet_type == 0:
+            length = int(binary_string[i:i+15],2)
+
+            i += (15 + length)
+        elif packet_type == 1:
+            number_subpackets = int(binary_string[i:i+11],2)
+            i += 11 * (number_subpackets + 1)
+
+            
+    return version_numbers
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -23,4 +83,4 @@ def day_16():
 if __name__ == "__main__":
 
 
-    day_16()
+    print(day_16())
